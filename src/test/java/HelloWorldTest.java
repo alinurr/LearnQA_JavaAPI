a1cf335f04ae62b3bf80cb8819efc519a4bcd40d
alinurr <alinurraiymbekova@gmail.com>
2023-07-30 14:47:34 +0600

    ParameterizedTest

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,11 +1,14 @@
 import io.restassured.RestAssured;
 import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelloWorldTest {
 
@@ -40,6 +43,16 @@ public class HelloWorldTest {
                 .andReturn();
         responseForCheck.print();
     }
+
+    @Test
+    public void testHelloMethodWithoutName(){
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+        String answer = response.getString("answer");
+        assertEquals("Hello, someone", answer, "The answer is not expected");
+    }
+
 
 
     @Test
